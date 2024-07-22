352ee371b1435b5f54019182b2217436c4cf25b5
Anpirur <angelaruizdev@gmail.com>
2024-07-22 09:01:05 +0200

    ClasesInternasLocal

--- clases_internas/PruebaTemporizador.java
+++ clases_internas/PruebaTemporizador.java
@@ -1,13 +1,14 @@
 package clases_internas;
+
 import javax.swing.JOptionPane;
 
 public class PruebaTemporizador {
 
-public static void main(String[] args) {
-    
-    Reloj miReloj= new Reloj(3000, true);
-    
-    miReloj.ejecutarTemporizador();
-    JOptionPane.showMessageDialog(null, "Pulsa ACEPTAR para terminar");
-}
+    public static void main(String[] args) {
+
+        Reloj miReloj = new Reloj(3000, true);
+
+        miReloj.ejecutarTemporizador();
+        JOptionPane.showMessageDialog(null, "Pulsa ACEPTAR para terminar");
+    }
 }
--- clases_internas/Reloj.java
+++ clases_internas/Reloj.java
@@ -9,33 +9,50 @@ import javax.swing.Timer;
 
 public class Reloj {
 
-    public  Reloj (int intervalo, boolean sonido){
-        this.intervalo=intervalo;
-        this.sonido=sonido;
+    public Reloj(int intervalo, boolean sonido) {
+        this.intervalo = intervalo;
+        this.sonido = sonido;
     }
 
-    public void ejecutarTemporizador(){
-        ActionListener oyente= new DameLaHora();
+    public void ejecutarTemporizador() {
+
+        /*
+         * SI QUEREMOS UNA CLASE INTERNA LOCAL DEBE SER DECLARADA DENTRO DEL METODO
+         * EJECUTAR TEMPORIZADOR Y SIN MODIFICADOR DE ACCESO.
+         * ADEMAS QUITARIAMOS LOS CAMPOS DE CLASE Y PASARIAN A SER PARAMETROS DE
+         * METODOS.
+         * Y PRESCINDIR DEL CONSTRUCTOR.
+         * 
+         * class DameLaHora implements ActionListener{
+         * 
+         * @Override
+         * public void actionPerformed(ActionEvent e) {
+         * 
+         * Date ahora = new Date ();
+         * System.out.println("Cada tres segundos tiene su hora: "+ahora);
+         * if (sonido )Toolkit.getDefaultToolkit().beep();
+         * }
+         */
+        ActionListener oyente = new DameLaHora();
         Timer miTemporizador = new Timer(intervalo, oyente);
         miTemporizador.start();
     }
 
-   
-
     private int intervalo;
-    private boolean sonido; 
+    private boolean sonido;
 
+    // HACEMOS UNA CLASE INTERNA CON MODIFICADOR PRIVATE (SOLO PARA CLASES INTERNAS)
 
-    //HACEMOS UNA CLASE INTERNA CON MODIFICADOR PRIVATE (SOLO PARA CLASES INTERNAS)
-private class DameLaHora implements ActionListener{
+    private class DameLaHora implements ActionListener {
 
-    @Override
-    public void actionPerformed(ActionEvent e) {
-       
-      Date ahora = new Date ();
-      System.out.println("Cada tres segundos tiene su hora: "+ahora);
-      if (sonido )Toolkit.getDefaultToolkit().beep();
-    }
-        
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+            Date ahora = new Date();
+            System.out.println("Cada tres segundos tiene su hora: " + ahora);
+            if (sonido)
+                Toolkit.getDefaultToolkit().beep();
+        }
+
     }
 }
