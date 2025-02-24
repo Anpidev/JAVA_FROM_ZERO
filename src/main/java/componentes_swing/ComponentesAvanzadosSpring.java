dd80b145ef1b97aeaca3576697c9674837bb8b59
Anpidev <angelaruizdev@gmail.com>
2025-02-24 23:54:58 +0100

    Componentes avanzados swing Spring layout

--- src/main/java/componentes_swing/ComponentesAvanzadosSpring.java
+++ src/main/java/componentes_swing/ComponentesAvanzadosSpring.java
@@ -7,6 +7,8 @@ package componentes_swing;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JButton;
+import javax.swing.SpringLayout;
+import javax.swing.Spring;
 
 
 public class ComponentesAvanzadosSpring {
@@ -35,11 +37,16 @@ class LaminaMuelle extends JPanel {
         JButton boton1=new JButton ("boton 1 ");
         JButton boton2 = new JButton ("boton 2");
         JButton boton3 = new JButton ("boton 3");
+        SpringLayout miLayout = new SpringLayout();
+        setLayout(miLayout);
         add(boton1);
         add(boton2);
         add(boton3);
-
-        
+        Spring muelle = Spring.constant(0, 10, 100);
+        miLayout.putConstraint(SpringLayout.WEST,boton1,muelle,SpringLayout.WEST,this);
+        miLayout.putConstraint(SpringLayout.WEST,boton2,muelle,SpringLayout.EAST,boton1);
+        miLayout.putConstraint(SpringLayout.WEST,boton3,muelle,SpringLayout.EAST,boton2);
+        miLayout.putConstraint(SpringLayout.EAST,this,muelle,SpringLayout.EAST,boton3);
 
     }
 
